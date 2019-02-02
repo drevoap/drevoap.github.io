b2dcb1be186f5754c0dd8249baca201aec3a6ce8
KitNewsNet <46684168+KitNewsNet@users.noreply.github.com>
2019-02-02 21:47:49 +0100

    Purchaser registration

---
+++ js/purchaser.js
@@ -0,0 +1,372 @@
+
+class PurchaserRegistrationSelectBox extends React.Component {
+    render() {
+        return (
+            <div className="row form-group">
+                <div className="col-md-12 mb-3 mb-md-0">
+                    <label className="font-weight-bold" htmlFor={this.props.id}>{this.props.text} {this.props.mandatory ? '*' : ''} </label>
+                    <select
+                        id={this.props.id}
+                        name={this.props.id}
+                        // value={props.value}
+                        className={this.props.classname}
+                        onChange={this.props.onchange}
+                    >
+                        {/*<option value="" disabled>{props.placeholder}</option>*/}
+                        {this.props.options.map(option => {
+                            return (
+                                <option
+                                    key={option.value}
+                                    value={option.value}
+                                    label={option.label}>{option.label}
+                                </option>
+                            );
+                        })}
+                    </select>
+                </div>
+            </div>)
+    }
+}
+
+
+class PurchaserRegistrationTextInput extends React.Component {
+    render() {
+        return (
+            <div className="row form-group">
+                <div className="col-md-12 mb-3 mb-md-0">
+                    <label className="font-weight-bold" htmlFor={this.props.id}>{this.props.text} {this.props.mandatory ? '*' : ''} </label>
+                    <input type="text" id={this.props.id} name={this.props.id} className={this.props.classname}
+                           placeholder={this.props.text} onChange={this.props.onchange}/>
+                    <div className="valid-feedback">
+                        {this.props.valid}
+                    </div>
+                    <div className="invalid-feedback">
+                        {this.props.invalid}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class PurchaserRegistrationForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            purchaser: {
+                fullname: '',
+                street: '',
+                city: '',
+                zip: '',
+                country: 'CZ',
+                ico: '',
+                vat: '',
+
+                contact: '',
+                phone: '',
+                email: ''
+            },
+
+            countries : [
+                { label: "Česká republika", value: 'CZ' },
+                { label: "Slovensko", value: 'SK' },
+                { label: "Polsko", value: "PL"}
+            ],
+
+            // slouzi k ulozeni chybove hlasky pro stranku
+            errors: {
+                fullname: '',
+                street: '',
+                city: '',
+                zip: '',
+                country: '',
+                ico: '',
+                vat: '',
+
+                contact: '',
+                phone: '',
+                email: ''
+            },
+            // slouzi i identifikaci zda byl jiz field validovan (rozlisit smazany a nezadany vstup)
+            validated: {
+                fullname: false,
+                street: false,
+                city: false,
+                zip: false,
+                country: false,
+                ico: false,
+                vat: false,
+
+                contact: false,
+                phone: false,
+                email: false
+            }
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleUserInput (event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState(prevState => ({ purchaser :
+                {...prevState.purchaser, [name]: value}
+        }), () => { this.validateField(name, value) });
+    }
+
+    validateField(fieldName, value) {
+        console.log("VALIDATION: Field " + fieldName + ": " + value);
+
+        switch(fieldName) {
+            case 'email':
+                this.state.validated.email = true;
+                var emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                if (value) {
+                    if (!emailValid) {
+                        this.setState(prevState => ({ errors :
+                                {...prevState.errors, [fieldName]: 'Email není validní.'}
+                        }));
+                        console.log('Email nevalidni');
+                        return false;
+                    } else {
+                        this.setState(prevState => ({ errors :
+                                {...prevState.errors, [fieldName]: null}
+                        }));
+                        console.log('Email validni');
+                        return true;
+                    }
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'Email musí být vyplněn.'}
+                    }));
+                    console.log('Email prazdny');
+                    return false;
+                }
+            case 'fullname':
+                this.state.validated.fullname = true;
+                if (value) {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: null}
+                    }));
+                    console.log('Fullname vyplnen');
+                    return true;
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'Název/jméno musí být vyplněno.'}
+                    }));
+                    console.log('Fullname prazdny');
+                    return false;
+                }
+            case 'street':
+                this.state.validated.street = true;
+                if (value) {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: null}
+                    }));
+                    console.log('Street vyplnen');
+                    return true;
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'Ulice musí být vyplněna.'}
+                    }));
+                    console.log('Street prazdny');
+                    return false;
+                }
+            case 'city':
+                this.state.validated.city = true;
+                if (value) {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: null}
+                    }));
+                    console.log('City vyplnen');
+                    return true;
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'Město musí být vyplněno.'}
+                    }));
+                    console.log('City prazdny');
+                    return false;
+                }
+            case 'zip':
+                this.state.validated.zip = true;
+                if (value) {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: null}
+                    }));
+                    console.log('Zip vyplnen');
+                    return true;
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'PSČ musí být vyplněno.'}
+                    }));
+                    console.log('Zip prazdny');
+                    return false;
+                }
+            case 'contact':
+                this.state.validated.contact = true;
+                if (value) {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: null}
+                    }));
+                    console.log('Contact vyplnen');
+                    return true;
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'Jméno kontaktní osoby musí být vyplněno.'}
+                    }));
+                    console.log('Contact prazdny');
+                    return false;
+                }
+            case 'phone':
+                this.state.validated.phone = true;
+                if (value) {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: null}
+                    }));
+                    console.log('Phone vyplnen');
+                    return true;
+                } else {
+                    this.setState(prevState => ({ errors :
+                            {...prevState.errors, [fieldName]: 'Telefon musí být vyplněn.'}
+                    }));
+                    console.log('Phone prazdny');
+                    return false;
+                }
+            default:
+                return true;
+        }
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let purchaserForm = this.state.purchaser;
+        let form_valid = true;
+
+        for (var purchaserField in purchaserForm) {
+            var field_validation = this.validateField(purchaserField, this.state.purchaser[purchaserField]);
+            console.log("Validace " + purchaserField + " skoncila: "+ field_validation);
+            if (!field_validation) {
+                form_valid = false;
+            }
+        }
+
+        if (form_valid) {
+            console.log(JSON.stringify(purchaserForm));
+
+            fetch('https://us-central1-harvestor-f8623.cloudfunctions.net/insertIntoDB/poptavka', {
+                method: 'POST',
+                body: JSON.stringify(purchaserForm),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                console.log(response.status);
+                if (response.status === 200) {
+                    // redirect na dekuji stranku
+                    window.location.href = 'registration_ok.html';
+                } else {
+                    // redirect na faild stranku
+                    window.location.href = 'registration_nok.html';
+                }
+            }).catch((error) => {
+                // totalni selhani
+                window.location.href = 'registration_nok.html';
+            });
+        }
+
+    }
+
+    getClassname(fieldname) {
+        if (this.state.validated[fieldname]) {
+            if (this.state.errors[fieldname]) {
+                return 'form-control is-invalid';
+            } else {
+                return 'form-control is-valid';
+            }
+        } else {
+            return 'form-control';
+        }
+    }
+
+    render() {
+        return (
+
+            <form id="purchaserform" onSubmit={this.handleSubmit} className="p-5 bg-white">
+
+                <h3>Obchodník</h3>
+                <PurchaserRegistrationTextInput id={'fullname'} text={'Jméno/název vlastníka'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.fullname}
+                                             classname={this.getClassname('fullname')}/>
+
+                <PurchaserRegistrationTextInput id={'street'} text={'Adresa - ulice a č.p.'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.street}
+                                             classname={this.getClassname('street')}/>
+
+                <PurchaserRegistrationTextInput id={'city'} text={'Adresa - město'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.city}
+                                             classname={this.getClassname('city')}/>
+
+                <PurchaserRegistrationTextInput id={'zip'} text={'Adresa - PSČ'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.zip}
+                                             classname={this.getClassname('zip')}/>
+
+                <PurchaserRegistrationSelectBox id={'country'} text={'Adresa - stát'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             classname={this.getClassname('country')}
+                                             options={this.state.countries}/>
+
+                <PurchaserRegistrationTextInput id={'ico'} text={'IČO'}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             classname={this.getClassname('ico')}/>
+
+                <PurchaserRegistrationTextInput id={'vat'} text={'DIČ'}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             classname={this.getClassname('vat')}/>
+
+                <h3>Kontaktní osoba</h3>
+                <PurchaserRegistrationTextInput id={'contact'} text={'Jméno osoby'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.contact}
+                                             classname={this.getClassname('contact')}/>
+
+                <PurchaserRegistrationTextInput id={'phone'} text={'Telefon'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.phone}
+                                             classname={this.getClassname('phone')}/>
+
+                <PurchaserRegistrationTextInput id={'email'} text={'Email'} mandatory={true}
+                                             onchange={(event) => this.handleUserInput(event)}
+                                             valid={'Email validní. Děkujeme za vyplnění'}
+                                             invalid={this.state.errors.email}
+                                             classname={this.getClassname('email')}/>
+
+
+
+                <div className="row form-group">
+                    <div className="col-md-12">
+                        <input type="submit" value="Send" className="btn btn-primary  py-2 px-4 rounded-0"/>
+                    </div>
+                </div>
+
+            </form>
+
+        );
+    }
+}
+
+ReactDOM.render(<PurchaserRegistrationForm/>, document.getElementById('purchaser_registration_form'));
