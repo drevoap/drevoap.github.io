5f2483a81d1f3c7b2db2b44915eee10a1eae190d
KitNewsNet <46684168+KitNewsNet@users.noreply.github.com>
2019-02-12 23:31:03 +0100

    Verification page

---
+++ js/verification.js
@@ -0,0 +1,29 @@
+function callVerfication() {
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var id = url.searchParams.get("id");
+    var entity = url.searchParams.get("entity");
+    console.log(id);
+    console.log(entity);
+
+    fetch('https://us-central1-harvestor-f8623.cloudfunctions.net/verification?id=' + id + "&entity=" + entity, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        mode: "cors"
+    }).then(response => {
+        console.log(response.status);
+        if (response.status === 200) {
+            // redirect na dekuji stranku
+            // window.location.href = 'registration_ok.html';
+        } else {
+            // redirect na faild stranku
+            // window.location.href = 'registration_nok.html';
+        }
+    }).catch((error) => {
+        // totalni selhani
+        // window.location.href = 'registration_nok.html';
+    });
+}
